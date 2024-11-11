ee99d783472f8b05ac8215d7a45df52b5a4e86a1
yenmangu <robert.shelford@googlemail.com>
2024-11-11 16:13:31 +0000

    [BGRP 20] Create report bug UI

---
+++ Bugreporter.Client/Features/ReportBug/ReportBugFormView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugreporter.Client.Features.ReportBug;
+
+public partial class ReportBugFormView : ContentView
+{
+    public ReportBugFormView()
+    {
+        InitializeComponent();
+    }
+}
